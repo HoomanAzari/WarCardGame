59c778d160a5cc85cbef145b9535cb4fc68088cc
Hooman Azari <hooazari@gmail.com>
2023-06-20 17:12:47 -0400

    Initial Commit

---
+++ WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Houman Azari on 2023-06-20.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
